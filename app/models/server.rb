295ae4b347a44a018b78d591cb73a9f33ff509c7
Eric Abbott <eric.abbott@viximo.com>
2011-02-21 11:24:49 -0500

    added cron entries for missing backup frequencies

--- app/models/server.rb
+++ app/models/server.rb
@@ -22,10 +22,13 @@ class Server < ActiveRecord::Base
 
   def register_cron_entries
     remove_all_cron_entries
-    CronEdit::Crontab.Add "hourly-"+ name, "4 * * * * "+ create_cron_commandline(id, 'hourly') if hourly?
-    CronEdit::Crontab.Add "daily-"+ name, "7 0 * * 1,2,3,4,5,6,7 "+ create_cron_commandline(id, 'daily') if daily?
-    CronEdit::Crontab.Add "weekly-"+ name, "12 0 1,8,15,22 * * "+ create_cron_commandline(id, 'weekly') if weekly?
-    CronEdit::Crontab.Add "monthly-"+ name, "15 0 1 * * "+ create_cron_commandline(id, 'monthly') if monthly?
+    CronEdit::Crontab.Add "minute-"+ name, "13,33,53 * * * * "+ create_cron_commandline(id, 'minute') if minute?
+    CronEdit::Crontab.Add "hourly-"+ name, "11 * * * * "+ create_cron_commandline(id, 'hourly') if hourly?
+    CronEdit::Crontab.Add "daily-"+ name, "9 0 * * 1,2,3,4,5,6,7 "+ create_cron_commandline(id, 'daily') if daily?
+    CronEdit::Crontab.Add "weekly-"+ name, "7 0 1,8,15,22 * * "+ create_cron_commandline(id, 'weekly') if weekly?
+    CronEdit::Crontab.Add "monthly-"+ name, "5 0 1 * * "+ create_cron_commandline(id, 'monthly') if monthly?
+    CronEdit::Crontab.Add "quarterly-"+ name, "3 0 1 1,4,7,10 * "+ create_cron_commandline(id, 'quarterly') if quarterly?
+    CronEdit::Crontab.Add "yearly-"+ name, "1 0 1 1 * "+ create_cron_commandline(id, 'yearly') if yearly?
   end
 
   def remove_all_cron_entries
