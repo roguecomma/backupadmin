1c435356fe5597d7084b278cd38b56c93174c3c7
Eric Abbott <eric.abbott@viximo.com>
2011-04-21 17:11:46 -0400

    widened the net, catching classes as opposed to text strings

--- spec/models/snapshot_spec.rb
+++ spec/models/snapshot_spec.rb
@@ -43,12 +43,12 @@ describe Snapshot do
   end
   
   describe '.report_action' do
-    it "should raise exception for known aws issue 1 #{Snapshot::AWS_FAILURE_1}" do
-      expect { Snapshot.report_action(@server, 'nuttin', 'no message') { raise Snapshot::AWS_FAILURE_1 } }.to raise_error(Snapshot::AWS_FAILURE_1)
+    it "should raise exception for known ec2 failure resulting in Fog service error" do
+      expect { Snapshot.report_action(@server, 'nuttin', 'no message') { raise Fog::Service::Error.new('hey') } }.to raise_error('hey')
     end
 
-    it "should raise exception for known aws issue 2 #{Snapshot::AWS_FAILURE_1}" do
-      expect { Snapshot.report_action(@server, 'nuttin', 'no message') { raise Snapshot::AWS_FAILURE_2 } }.to raise_error(Snapshot::AWS_FAILURE_2)
+    it "should raise exception for known ec2 failure resulting in excon error" do
+      expect { Snapshot.report_action(@server, 'nuttin', 'no message') { raise Excon::Errors::SocketError.new(StandardError.new('whatup')) } }.to raise_error('whatup')
     end
 
     it "should not raise exception for any non-known exception" do
@@ -57,11 +57,6 @@ describe Snapshot do
   end
   
   describe '.recent_untagged_snapshot_found_and_processed' do
-    before(:each) do
-      #@aws_snapshot = AWS.snapshots.create(:volume_id => @volume.id).reload
-      #@snapshot = Snapshot.new(@server, @aws_snapshot)
-    end
-
     it 'should do nothing if most recent snapshot properly tagged' do
       @aws_snapshot = AWS.snapshots.create(:volume_id => @volume.id)
       @aws_snapshot.tags = {Snapshot::NAME_TAG => 'something'}
@@ -73,7 +68,7 @@ describe Snapshot do
     it 'should do nothing if most recent snapshot created too old' do
       Timecop.freeze(10.minutes.ago) { @aws_snapshot = AWS.snapshots.create(:volume_id => @volume.id) }
       @server.stub!(:volume_id).and_return(@aws_snapshot.volume_id)
-      Snapshot.should_receive(:add_initial_tags)
+      Snapshot.should_not_receive(:add_initial_tags)
       Snapshot.recent_untagged_snapshot_found_and_processed!(@server, 'minute')
     end
 
